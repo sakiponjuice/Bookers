16213cf6b9e9620da1e4ec9042b0e37354214d35
sakiponjuice <saki.pon.juice0922@gmail.com>
2020-11-07 10:34:08 +0000

    [Add] トップページと投稿一覧画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/' => 'bookers#top'
+  get 'books'  => 'bookers#index'
+  post 'books' => 'bookers#create'
+  get 'books/show' => 'bookers#show'
+  get 'books/edit' => 'bookers#edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
