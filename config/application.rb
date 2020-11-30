7b9a299d03b4eb20c6a3b2d921970399f47fc96f
sakiponjuice <saki.pon.juice0922@gmail.com>
2020-11-30 11:28:19 +0000

    [Add] バッチ処理

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Bookers
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.paths.add 'lib', eager_load: true
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
